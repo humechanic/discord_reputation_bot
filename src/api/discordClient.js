eaa07ec770e4888deb4d31b0093268b121255d00
Humechanic <j4nuary@mail.ru>
2024-10-30 14:31:54 +0300

    feat: get users, get channels, get roles

--- src/api/discordClient.js
+++ src/api/discordClient.js
@@ -1,3 +1,18 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 
-export const discordClient = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildIntegrations] });
+export const discordClient = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildEmojisAndStickers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessagePolls,
+    ]
+});

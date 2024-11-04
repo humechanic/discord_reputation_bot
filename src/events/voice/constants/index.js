0170ba6cfff87b830ee405e792800c1652042101
Humechanic <j4nuary@mail.ru>
2024-11-04 19:37:22 +0300

    feat: add role sound

--- src/events/voice/constants/index.js
+++ src/events/voice/constants/index.js
@@ -1,3 +1,4 @@
 import path from 'path';
 
-export const soundFilePath = path.join(process.cwd(), '/src/assets', 'admin_join.mp3');
+export const adminFilePath = path.join(process.cwd(), '/src/assets', 'admin_join.mp3');
+export const bodyaFilePath = path.join(process.cwd(), '/src/assets', 'bodya_join.mp3');
